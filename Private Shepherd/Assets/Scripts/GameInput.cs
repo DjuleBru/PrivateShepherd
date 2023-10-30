e9c4d184e7b3fef7f9ffa06f74611a016edd0527
DjuleBru <julien-taconet@hotmail.com*>
2023-10-30 12:28:18 +0100

    test

--- Private Shepherd/Assets/Scripts/GameInput.cs
+++ Private Shepherd/Assets/Scripts/GameInput.cs
@@ -24,50 +24,9 @@ public class GameInput : MonoBehaviour
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
 
-        playerInputActions.Player.Bark.performed += Bark_performed;
-        playerInputActions.Player.Bark.canceled += Bark_released;
-        playerInputActions.Player.Bark.started += Bark_started;
-
-        playerInputActions.Player.Growl.performed += Growl_performed;
-        playerInputActions.Player.Growl.canceled += Growl_canceled;
-
-        playerInputActions.Player.Run.performed += Run_performed;
-        playerInputActions.Player.Run.canceled += Run_canceled;
-
-        playerInputActions.Player.Exit.performed += Exit_performed;
     }
 
-    private void Bark_started(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
-        OnBarkPressed?.Invoke(this, EventArgs.Empty);
-    }
-
-    private void Exit_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
-        OnExitPerformed?.Invoke(this, EventArgs.Empty);
-    }
-
-    private void Run_canceled(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
-        OnRunReleased?.Invoke(this, EventArgs.Empty);
-    }
-
-    private void Run_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
-        OnRunPerformed?.Invoke(this, EventArgs.Empty);
-    }
-
-    private void Growl_canceled(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
-        OnGrowlReleased?.Invoke(this, EventArgs.Empty);
-    }
-
-    private void Growl_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
-        OnGrowlPerformed?.Invoke(this, EventArgs.Empty);
-    }
-
-    private void Bark_released(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
-        OnBarkReleased?.Invoke(this, EventArgs.Empty);
-    }
-
-    private void Bark_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
-        OnBarkPerformed?.Invoke(this, EventArgs.Empty);
-    }
+    
 
     public Vector2 GetMovementVector() {
         Vector2 moveInput = playerInputActions.Player.Move.ReadValue<Vector2>();
